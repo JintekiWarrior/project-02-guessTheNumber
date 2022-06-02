d2b71bda4ecad7989882537a3498ef1610f6db5a
JintekiWarrior <jintekiwarrior@gmail.com>
2022-06-02 10:37:50 -0400

    Add exception error handling for input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,14 @@ public class Main {
         {
             gameInit();
             System.out.println("Would you like to play again? (y or n)");
-            char playAgain = gameScanner.next().charAt(0);
+            char playAgain = 'A';
+            try
+            {
+                playAgain = gameScanner.next().charAt(0);
+            } catch (Exception err)
+            {
+                System.out.println(err.getClass().getName());
+            }
             char no = 'n';
             if (playAgain == no)
             {
@@ -36,16 +43,37 @@ public class Main {
 
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Hello, what be thy name?");
-        String name = myScanner.nextLine();
+        String name = "";
+
+        // Input exception handling
+        try
+        {
+            name = myScanner.nextLine();
+        } catch (Exception err)
+        {
+            System.out.println(err.getClass().getName());
+        }
+
         System.out.println("Sup, " + name + " guess a number between 1 and 20 if you want to live.");
 
         while (guess != randomNumber && count > 0)
         {
+            // Want this to run after the first loop.
             if (guess != 0)
             {
                 System.out.println("You have " + count + " tries left. Try again");
             }
-            guess = myScanner.nextInt();
+
+            try
+            {
+                guess = myScanner.nextInt();
+            }
+            catch (Exception err)
+            {
+                System.out.println(err.getClass().getName());
+                myScanner.nextLine();
+            }
+
             if (guess > randomNumber)
             {
                 System.out.println("Too high");
