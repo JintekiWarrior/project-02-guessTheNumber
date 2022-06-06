eacf6f8a2e8cfa9f2f1b804d0ad072df08a5b8f4
JintekiWarrior <jintekiwarrior@gmail.com>
2022-06-06 08:38:44 -0400

    Add try and catch statments to scanner

--- src/java/GuessNumberGame.java
+++ src/java/GuessNumberGame.java
@@ -26,6 +26,7 @@ public class GuessNumberGame {
         return this.guesses;
     }
 
+    // Player has a finite amount of guesses
     public int getGuessAmountLeft()
     {
         return this.guessAmountLeft;
@@ -36,6 +37,7 @@ public class GuessNumberGame {
         return this.randomNumber;
     }
 
+    // Check to match the player guess against the random number generated
     public boolean checkPlayerGuess(int guess)
     {
         if (guess < 1 || guess > 20)
@@ -50,9 +52,15 @@ public class GuessNumberGame {
         {
             System.out.println("You guessed too high");
             return false;
+        } else if (guess == this.randomNumber)
+        {
+            System.out.println("Congrats you guessed it!");
+            return true;
+        } else
+        {
+            System.out.println("You have an error");
+            return false;
         }
-        System.out.println("Congrats you guessed it!");
-        return true;
     }
 
     public void setPlayerGuess(int guess)
@@ -69,4 +77,5 @@ public class GuessNumberGame {
     {
         this.guessAmountLeft--;
     }
+
 }
--- src/java/Main.java
+++ src/java/Main.java
@@ -7,7 +7,15 @@ public class Main {
         boolean playAgain = true;
 
         System.out.println("What is your name?");
-        String name = gameScanner.nextLine();
+        String name = "";
+
+        try
+        {
+            name = gameScanner.nextLine();
+        } catch(Exception err)
+        {
+            System.out.println(err.getClass().getName());
+        }
 
         while (playAgain)
         {
@@ -15,19 +23,37 @@ public class Main {
             System.out.println("Hello " + game.getPlayerName() + " Guess a number between 1 and 20.");
             for (int i = game.getGuessAmountLeft(); i > 0; i--)
             {
-                int guess = gameScanner.nextInt();
+                int guess = 0;
+                try
+                {
+                    guess = gameScanner.nextInt();
+                } catch(Exception err)
+                {
+                    System.out.println(err.getClass().getName());
+                }
+
+
                 boolean check = game.checkPlayerGuess(guess);
                 if (check)
                 {
                     break;
                 }
+                gameScanner.nextLine();
             }
             // reset scanner
             gameScanner.nextLine();
 
             System.out.println("Would you like to play again? (y or n)");
             char yes = 'y';
-            char playerResponse = gameScanner.next().charAt(0);
+            char playerResponse = 'a';
+
+            try
+            {
+                playerResponse = gameScanner.next().charAt(0);
+            } catch(Exception err)
+            {
+                System.out.println(err.getClass().getName());
+            }
 
             // if player types anything but y the game closes
             if (playerResponse != yes)
